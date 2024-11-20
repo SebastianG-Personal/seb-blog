064df825243916795009ba6ce6ad75318ef64d26
Sebastian Grobelny <sgrobelny@Sebastians-MBP.MG8702>
2024-11-20 13:22:33 -0800

    Next config update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    appDir: true, // Ensures the app directory is enabled
-  },
+  reactStrictMode: true, // Optional, enables React's strict mode
+  swcMinify: true, // Optional, enables SWC for minification
 };
 
 export default nextConfig;
