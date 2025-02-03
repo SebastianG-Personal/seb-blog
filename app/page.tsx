59eea25b3284ee5957286f0fca7cf7a0dcfb63c5
Sebastian Grobelny <sgrobelny@Sebastians-MBP.MG8702>
2025-02-03 12:05:26 -0800

    Add some pages to personal site

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
+
+// Navbar component
+function Navbar() {
+  return (
+    <nav className="fixed top-0 flex justify-between p-4 bg-gray-800 text-white w-full">
+      <Link href="/" className="hover:underline">Home</Link>
+      <Link href="projects/" className="hover:underline">Projects</Link>
+      <Link href="pilot/" className="hover:underline">Pilot</Link>
+    </nav>
+  );
+}
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+    <div className="grid grid-rows-[auto_20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Navbar />
+      <main className="flex flex-col gap-8 row-start-3 items-center sm:items-start">
       Hello this is Sebastian and I am a Senior Engineering Director at Accolade.
       </main>
     </div>
